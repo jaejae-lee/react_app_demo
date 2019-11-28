85d8299b6dedb44cb1496a290d44be3a0be7e32c
jae.L <jklee900414@gmail.com>
2019-11-28 17:22:07 +0000

    display square using ternary operator - true/false

--- src/jsx/Stuff.js
+++ src/jsx/Stuff.js
@@ -5,13 +5,17 @@ import Para from './Paragraph';
 import Square from './Square';
 import People from './People';
 
-const Stuff = (square) => (
+const Stuff = ( { square }) => (
     <>
         <Header>Hello, World</Header>
-        <Para>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos.</Para>
-        <Square color = 'lime' square = 'square' />
+        <Para>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nogs ad agendum esse natos.</Para>
+        { square ? (<Square color= "lime" />) : null }
         <People names = { ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"] } />
     </>
   );
 
-  export default Stuff;
+Stuff.defaultProps = {
+    square: true,
+};
+
+export default Stuff;
