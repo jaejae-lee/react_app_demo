227f4efc0ae3f74f498ec65ab4d3d4fb97e31e93
jae.L <jklee900414@gmail.com>
2019-11-28 17:07:42 +0000

    props exercise

--- src/jsx/Stuff.js
+++ src/jsx/Stuff.js
@@ -5,13 +5,13 @@ import Para from './Paragraph';
 import Square from './Square';
 import People from './People';
 
-const Stuff = () => (
-    <React.Fragment>
-        <Header />
-        <Para />
-        <Square />
-        <People />
-    </React.Fragment>
+const Stuff = (square) => (
+    <>
+        <Header>Hello, World</Header>
+        <Para>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos.</Para>
+        <Square color = 'lime' square = 'square' />
+        <People names = { ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"] } />
+    </>
   );
 
   export default Stuff;
