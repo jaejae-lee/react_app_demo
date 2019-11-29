b00846fc7f78f7ac791b1430a70cdb49153e28c1
jae.L <jklee900414@gmail.com>
2019-11-29 12:10:21 +0000

    change from functional comp to class compo

--- src/jsx/Header.js
+++ src/jsx/Header.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = ({ children }) => (
-    <h1>{ children }</h1>
-  );
+// const Header = ({ children }) => (
+//     <h1>{ children }</h1>
+//   );
 
+// export default Header;
+
+class Header extends Component {
+
+  render() { 
+
+    let { children } = this.props;
+
+    return ( 
+      <h1>{ children }</h1>
+    );
+  }
+}
+ 
 export default Header;
