a4a458567c5dc3da5d4d4072ffa5999b34bd3363
jae.L <jklee900414@gmail.com>
2019-11-29 12:12:30 +0000

    change from functional comp to class compo

--- src/jsx/Paragraph.js
+++ src/jsx/Paragraph.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Para extends Component {
 
-const Para = ({ children }) => (
-    <p>{ children }</p>
-);
+    render() { 
+
+        let { children } = this.props;
+
+        return (
+            <p>{ children }</p>
+        );
+    }
+}
 
 Para.defaultProps = {
     children: "Hello, world"
-};
+  };
   
 export default Para;
