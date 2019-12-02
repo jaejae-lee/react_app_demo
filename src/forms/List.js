df8327431b985e4ec0195e40448f5a787ead4f78
jae.L <jklee900414@gmail.com>
2019-12-02 13:05:08 +0000

    print user input items on the list

--- src/forms/List.js
+++ src/forms/List.js
@@ -5,14 +5,21 @@ class List extends Component {
     constructor(props){
         super(props);
         this.state = { 
-            input: ""
+            input: "",
+            list: [],
          }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
+
     handleChange(e) {
-        this.setState({ input: e.currentTarget.value });
+        this.setState({input: e.currentTarget.value,});
+    }
+
+    handleClick(e) {
+        this.setState({ list: [...this.state.list, this.state.input ] });
     }
 
     render() { 
@@ -23,10 +30,12 @@ class List extends Component {
                 value={ this.state.input }
                 className="form-control"
                 />
+
                 <ul>
-                    <li>{ this.state.input.value }</li>
+                    { this.state.list.map((Listitem, i) => <li key = { i }> { this.state.list[i] } </li> ) }
                 </ul>
-                <button onClick = { this.handleChange } >Add</button>
+
+                <button onClick = { this.handleClick } >Add</button>
             </div>
         );
     }
